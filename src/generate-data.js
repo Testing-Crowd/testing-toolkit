dfa0b9cff013e2c95a07613ddbad1238660e7674
Shaun Swales <shaun.swales@hopin.to>
2021-04-30 13:55:54 +0100

    Removed unwanted imports.

--- src/generate-data.js
+++ src/generate-data.js
@@ -1,6 +1,5 @@
 const generateUUID = require('uuid/v4');
 const faker = require('faker');
-const ipCountries = require('../data/ip-countries.json');
 
 const generateID = () => generateUUID();
 
@@ -21,13 +20,6 @@ const generateIPAddress = (country) => {
   if (!country) {
     return faker.internet.ip();
   }
-  country = country.toUpperCase();
-  if (!ipCountries[country]) {
-    throw new Error(`Country code ${country} is not defined`);
-  }
-  return ipCountries[country][
-    faker.random.number({ min: 0, max: ipCountries[country].length - 1 })
-  ];
 };
 
 module.exports = {
