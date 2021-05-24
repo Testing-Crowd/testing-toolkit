09bb1a34b2f5b190b33519535d3fd71fc5bb4d2f
Shaun Swales <shaun.swales@hopin.to>
2021-05-24 14:42:02 +0100

    ESLint

--- test/log.test.js
+++ test/log.test.js
@@ -2,27 +2,25 @@ const { afterEach } = require('jest-circus');
 const { log } = require('../index');
 
 describe('Logging', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
-    beforeEach(() => {
-        jest.clearAllMocks();
-    });
+  test('log string', () => {
+    const spy = jest.spyOn(log, 'info').mockImplementation((msg) => msg);
 
-    test('log string', () => {
-        const spy = jest.spyOn(log, 'info').mockImplementation((msg) => msg);
+    log.info('Is this on?');
 
-        log.info('Is this on?');
+    expect(spy).toHaveBeenCalled();
+    expect(spy).toBeCalledWith('Is this on?');
+  });
 
-        expect(spy).toHaveBeenCalled();
-        expect(spy).toBeCalledWith('Is this on?');
-    });
+  test('log object', () => {
+    const spy = jest.spyOn(log, 'debug').mockImplementation((msg) => msg);
 
-    test('log object', () => {
-        const spy = jest.spyOn(log, 'debug').mockImplementation((msg) => msg);
+    log.debug({ id: 1 }, 'Some message');
 
-        log.debug({ id: 1 }, 'Some message');
-
-        expect(spy).toHaveBeenCalled();
-        expect(spy).toBeCalledWith({ id: 1 }, 'Some message');
-    });
-
+    expect(spy).toHaveBeenCalled();
+    expect(spy).toBeCalledWith({ id: 1 }, 'Some message');
+  });
 });
